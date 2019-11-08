684a14459bf4b902357c6c72031f1c94957d71ba
Marc Jacobi <marc.jacobi@macaw.nl>
2019-11-08 17:45:25 +0100

    antlr runtime and vs proj

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,14 @@
-int main()
+#include <iostream>
+#include "zsharp_parserLexer.h"
+#include "zsharp_parserParser.h"
+
+int main(int argc, char **argv)
 {
+    if (argc != 2) {
+        std::cout << "Specify a file to parse.";
+        return 0;
+    }
+
+
     return 0;
 }
